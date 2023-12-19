4bbd804be03287080bf57714a8eb7a9f2e315d9e
Harsh Wadhwa <hwadhwa050@gmail.com>
2023-12-19 15:43:30 +0530

    Initial commit

---
+++ src/app/components/navbar.jsx
@@ -0,0 +1,74 @@
+"use client";
+import Link from 'next/link';
+import React, { useState } from 'react';
+import  NavLink from './NavLink';
+import { Bars3Icon } from "@heroicons/react/24/outline";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RxCross1 } from "react-icons/rx";
+import MenuOverlay from './MenuOverlay';
+
+const navLinks = [
+    {
+        title: "About",
+        path: "#about",
+    },
+    {
+        title: "Projects",
+        path: "#projects",
+    },
+    {
+        title: "Contact",
+        path: "#contact",
+    },
+];
+
+const navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);  
+  return (
+    <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100'>
+        <div className='flex flex-wrap items-center justify-between mx-auto px-4 py-2'>
+            <Link href={"/"} className='text-2xl md:text-5xl text-white font-semibold'>
+                LOGO
+            </Link>
+            <div className='mobile-menu block md:hidden'>
+                {
+                    !navbarOpen ? (
+                        <button onClick={() => setNavbarOpen(true)} 
+                                className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'>
+                            <Bars3Icon className='h-5 w-5' />
+                            {/* <RxCross1 color="white" size={20}/> */}
+                            
+                        </button>
+                    ) : (
+                        <button onClick={() => setNavbarOpen(false)} 
+                                className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'>
+                            <XMarkIcon className='h-5 w-5' />
+                            {/* <GiHamburgerMenu color="white" size={20}/> */}
+                        </button>
+                    )
+                }
+            </div>
+            <div className='menu hidden md:block md:w-auto' id='navbar'>
+                <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0' >
+                    {
+                        navLinks.map((link, index) => (
+                            <li key={index}>
+                                <NavLink href={link.path} title={link.title} />
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
+        </div>
+        {navbarOpen ? <MenuOverlay links={navLinks} /> : null }
+    </nav>
+  )
+}
+
+export default navbar;
+
+
+
+
+
