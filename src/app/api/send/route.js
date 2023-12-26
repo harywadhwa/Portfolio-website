2db1006928eb99c15981d57eb70ae7b0ac38ac9f
Harsh Wadhwa <hwadhwa050@gmail.com>
2023-12-26 14:24:24 +0530

    added new section

---
+++ src/app/api/send/route.js
@@ -0,0 +1,25 @@
+// import { EmailTemplate } from '../../../components/EmailTemplate';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+  try {
+    const data = await resend.emails.send({
+      from: 'Harsh <hwadhwa050@gmail.com>',
+      to: ['hwadhwa050@gmail.com'],
+      subject: 'Hello world',
+      react: (
+        <>
+            <p>
+                Email Body
+            </p>
+        </>
+      ),
+    });
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error });
+  }
+}
