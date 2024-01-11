84c6b709cb08da697e12e5b567829c74436946e5
Harsh Wadhwa <hwadhwa050@gmail.com>
2024-01-11 16:16:12 +0530

    Added auth

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,6 +1,9 @@
 // import { EmailTemplate } from '../../../components/EmailTemplate';
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
+resend.apiKeys.create({ name: 'Production' });
+Authorization:  RESEND_API_KEY;
+
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
